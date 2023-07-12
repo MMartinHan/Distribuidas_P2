16b279e79d11d7291882c1e02d2cb87dd9a482be
ArielGonzalezH <aagonzalez8@espe.edu.ec>
2023-07-12 16:29:07 -0500

    Actualizar eliminar motivo

--- Server/servidor.py
+++ Server/servidor.py
@@ -47,4 +47,19 @@ while True:
         print(resultado)
         print(type(resultado))
         conexion.send(resultado.encode("utf-8"))
+    elif sql_query[0]=="MODIFICAR":
+        print("Modificar")
+        sql = "UPDATE " + sql_query[1] + " SET NOMBRE_MOT='" + sql_query[2] + "' WHERE CODIGO_MOT=" + sql_query[3]
+        print(sql)
+        if per.persistencia(sql) == True:
+            conexion.send("Orden ejecutada con exito".encode("utf-8"))
+        else:
+            conexion.send("Error al ejecutar la orden".encode("utf-8"))
+    elif sql_query[0]=="ELIMINAR":
+        print("Eliminar")
+        sql = "DELETE FROM " + sql_query[1] + " WHERE CODIGO_MOT=" + sql_query[2]
+        if per.persistencia(sql) == True:
+            conexion.send("Orden ejecutada con exito".encode("utf-8"))
+        else:
+            conexion.send("Error al ejecutar la orden".encode("utf-8"))
     conexion.close()
