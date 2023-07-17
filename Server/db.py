36e7f753ade1a5c097f95fbab7334f9bfb515ec9
ArielGonzalezH <aagonzalez8@espe.edu.ec>
2023-07-17 18:25:08 -0500

    Eliminar asientos

--- Server/db.py
+++ Server/db.py
@@ -2,11 +2,7 @@ import mysql.connector
 
 config = {
     'user': 'root',
-<<<<<<< HEAD
-    'password': 'root',
-=======
     'password': 'aagonzalez8',
->>>>>>> 7e3dc7d19b547334eba1e23bcec938b5bcd8a58d
     'host': 'localhost',
     'database': 'proyectod',
 }
