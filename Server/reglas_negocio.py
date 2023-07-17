9aef698e747eb12043ce15af9e6fdf6de95550cc
ArielGonzalezH <aagonzalez8@espe.edu.ec>
2023-07-17 12:23:59 -0500

    Disenio modificar asiento

--- Server/reglas_negocio.py
+++ Server/reglas_negocio.py
@@ -18,3 +18,24 @@ def verificar_asiento(cantidad_debe, cantidad_haber):
         return True
     else:
         return False
+
+def calcular13(sueldo):
+    return sueldo / 12
+
+def calcular14(sueldo):
+    return 450 / 12
+
+def calcularGastoFondoReserva(sueldo):
+    return sueldo / 12
+
+def calcularAportePatronal(sueldo):
+    return sueldo * 0.1215
+
+def aportePersonalPagar(sueldo):
+    return sueldo * 0.0945
+
+def aportePatronalPagar(sueldo):
+    return sueldo * 0.1215
+
+def nominaPagar(sueldo):
+    return (sueldo+calcular13(sueldo)+calcular14(sueldo)+calcularGastoFondoReserva(sueldo)+calcularAportePatronal(sueldo))-(aportePersonalPagar(sueldo)+aportePatronalPagar(sueldo)+nominaPagar(sueldo))
