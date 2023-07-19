e2e4626a4dcf620d827cc52eb1a4cdc7f27cad5e
MMartinHan <mmhan@espe.edu.ec>
2023-07-19 15:59:30 -0500

    Calculo de calificacion

--- Server/db.py
+++ Server/db.py
@@ -2,11 +2,7 @@ import mysql.connector
 
 config = {
     'user': 'root',
-<<<<<<< HEAD
-    'password': 'aagonzalez8',
-=======
     'password': '12345',
->>>>>>> b1d94f8d4c02d93564d58c36f306f0116171f8ef
     'host': 'localhost',
     'database': 'proyectod',
 }
